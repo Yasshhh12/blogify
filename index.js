a41afac567b2a667b2677b0fa87421b600a54c62
Yasshhh12 <prasyast152@gmail.com>
2024-08-12 20:25:40 +0530

    Created Partials

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const app = express();
 const PORT = 8000;
 
 app.set("view engine","ejs");
-app.set("views",path.resolve("./views"))
+app.set("views",path.resolve("./views"));
+
+app.get('/',(req,res)=>{
+    res.render('home');
+});
 
 app.listen(PORT,()=>console.log(`Server started at PORT:${PORT}`));
 
