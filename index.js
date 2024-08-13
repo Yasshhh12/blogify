1e3e23c00f7509996d3ab925a09a51d582cdf645
Yasshhh12 <prasyast152@gmail.com>
2024-08-13 18:42:43 +0530

    Added middleware for hash/salt password

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = 8000;
 app.set("view engine","ejs");
 app.set("views",path.resolve("./views"));
 
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended:false}));
 
 app.get('/',(req,res)=>{
     res.render('home');
